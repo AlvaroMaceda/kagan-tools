0b8f8c84b9226aeae86fb86743a52545dcc38816
Alvaro Maceda <alvaro@alvaromaceda.es>
2018-09-13 22:36:44 +0200

    Drawing pies

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ let start$ = fromEvent(startButton,'click');
 let spinnerCanvasElement = document.getElementById("spinner-canvas");
 let spinnerCanvas = spinnerCanvasElement.getContext("2d");
 
-function adjustCanvasToContainer(canvasElement, drawingContext) {
+function adjustCanvasToContainer(canvasElement, ctx) {
     let container = canvasElement.parentElement;
     let positionInfo = container.getBoundingClientRect();
 
@@ -23,15 +23,57 @@ function adjustCanvasToContainer(canvasElement, drawingContext) {
     canvasElement.width = width;
     canvasElement.height = height;
 
-    drawingContext.width = width;
-    drawingContext.height = height;
+    ctx.width = width;
+    ctx.height = height;
 }
-adjustCanvasToContainer(spinnerCanvasElement,spinnerCanvas);
-function setCartesianCoordinates(drawingContext) {
-    drawingContext.translate(drawingContext.width/2,drawingContext.height/2); // now 0,0 is the center of the canvas.
+function setOriginInCanvasCenter(ctx) {
+    ctx.translate(ctx.width/2,ctx.height/2); // now 0,0 is the center of the canvas.
 }
-setCartesianCoordinates(spinnerCanvas);
 
+adjustCanvasToContainer(spinnerCanvasElement,spinnerCanvas);
+setOriginInCanvasCenter(spinnerCanvas);
+
+
+function angle(angle) {
+    return angle - ((1/2) * Math.PI);;
+}
+
+function pie(ctx,{x = 0, y= 0, radius, start = 0, end = 2* Math.PI, lineColor, linewidth= 1, fillColor}) {
+    console.log(`x:${x} radius=${radius} stroke=${lineColor}`);
+
+    ctx.save();
+
+    if(lineColor!==undefined) ctx.strokeStyle = lineColor;
+    if(fillColor!==undefined) ctx.fillStyle = fillColor;
+    ctx.lineWidth = linewidth;
+
+    ctx.moveTo(x, y);
+    ctx.arc(x,y,radius,angle(start),angle(end));
+    ctx.lineTo(x, y);
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.restore();
+}
+
+function patata(ctx) {
+    ctx.save();
+
+    // console.log(angle(0));
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.arc(0,0,50,angle(0),angle(Math.PI));
+    ctx.moveTo(0, 0);
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'red';
+    // ctx.stroke();
+    ctx.fill();
+
+    ctx.restore();
+}
+// patata(spinnerCanvas);
+pie(spinnerCanvas,{x:0,y:0,start:0,end:Math.PI,radius:50,fillColor:'red',lineColor:'black'});
 
 
 //709

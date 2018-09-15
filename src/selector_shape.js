c9e1bba5dc6be2f54d0163ae0ee40f61396276d3
Alvaro Maceda <alvaro@alvaromaceda.es>
2018-09-15 19:15:04 +0200

    First approximation to labels

--- src/selector_shape.js
+++ src/selector_shape.js
@@ -2,32 +2,6 @@ import { PolarPoint } from './geometry';
 
 const FULL_CIRCLE = 2*Math.PI;
 
-export class SelectorShape {
-
-    constructor() {
-    }
-
-    draw(ctx, center) {
-
-        let styleOdd = new DrawStyle({fillColor:'red',lineColor:'black',lineWidth:2});
-        let styleEven = new DrawStyle({fillColor:'white',lineColor:'black',lineWidth:2});
-
-        setDrawStyle(ctx,styleEven);
-        dividedCircle(ctx,{center, radius:130, numParts:5});
-
-        setDrawStyle(ctx,styleOdd);
-        dividedCircle(ctx,{center, radius:100, numParts:4});
-
-        setDrawStyle(ctx,styleEven);
-        dividedCircle(ctx,{center, radius:70, numParts:3});
-
-        setDrawStyle(ctx,styleOdd);
-        dividedCircle(ctx,{center, radius:40, numParts:2});
-
-    }
-
-}
-
 class DrawStyle {
     constructor({lineColor, lineWidth, fillColor}) {
         this.lineColor = lineColor;
@@ -36,25 +10,90 @@ class DrawStyle {
     }
 }
 
+let styleOdd = new DrawStyle({fillColor:'red',lineColor:'black',lineWidth:2});
+let styleEven = new DrawStyle({fillColor:'white',lineColor:'black',lineWidth:2});
+
+export class SelectorShape {
+
+    constructor() {
+    }
+
+    draw(ctx, center, radius) {
+
+
+        ctx.fillStyle = 'black';
+
+        ring(ctx, styleEven, {center, innerRadius:90, outerRadius:120, numParts:5});
+        ring(ctx, styleOdd, {center, innerRadius:60, outerRadius:90, numParts:4});
+        ring(ctx, styleEven, {center, innerRadius:30, outerRadius:60, numParts:3});
+        ring(ctx, styleOdd, {center, innerRadius:0, outerRadius:30, numParts:2});
+    }
+
+}
+
+function ring(ctx, drawStyle, {center, innerRadius, outerRadius, numParts}) {
+    setDrawStyle(ctx,drawStyle);
+    dividedCircle(ctx,{center, radius:outerRadius, numParts});
+    dividedCircleLabels(ctx,{center, radius:outerRadius, numParts});
+}
+
+
 function setDrawStyle(ctx, drawStyle) {
     if(drawStyle.lineColor !== undefined) ctx.strokeStyle = drawStyle.lineColor;
     if(drawStyle.fillColor !== undefined) ctx.fillStyle = drawStyle.fillColor;
     if(drawStyle.lineWidth !== undefined) ctx.lineWidth = drawStyle.lineWidth;
 }
 
-
 function dividedCircle(ctx, {center, radius, numParts = 2}) {
 
-    let startAngle = 0;
+    let parts = circleParts(numParts);
+
+    parts.forEach(({start, end}) => {
+        pie(ctx, {center, radius, start, end});
+    });
+
+}
+
+function dividedCircleLabels(ctx, {center, radius, numParts = 2}) {
+
+    ctx.textAlign = 'center';
+
+    let start = FULL_CIRCLE / (2 * numParts);
+    let parts = circleParts(numParts, start);
+    let i = 1;
+
+    parts.forEach(({start}) => {
+        let position = new PolarPoint(radius,translateAngleToOriginOnTop(start)).toCartesian();
+        ctx.fillText(i++,position.x,position.y);
+    });
+
+}
+
+function partsOfInterval(){
+    // This should be a generalization of circleParts
+}
+function displace() {
+    // To use as:
+
+    // displaceX = (interval) => displace(interval, X)
+    // parts = partsOfInterval(0,2*Math.PI,parts).foreach(displaceX)
+}
+
+
+function circleParts(numParts, start = 0) {
+
+    let startAngle = start;
     let increment = FULL_CIRCLE / numParts;
     let endAngle;
+    let result = [];
 
     for(let i=0; i<numParts; i++) {
         endAngle = startAngle + increment;
-        pie(ctx, {center, radius, start:startAngle, end: endAngle});
+        result.push({start:startAngle, end: endAngle});
         startAngle = endAngle;
     }
 
+    return result;
 }
 
 function pie(ctx,{center, radius, start = 0, end = 2* Math.PI}) {
